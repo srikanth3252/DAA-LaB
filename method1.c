95a61329df6480aad6f61c98254731d4e153f360
srikanth3252 <srikanthreddybapatu04@gmail.com>
2025-08-20 15:48:32 +0530

    summation

---
+++ method1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{   
+     int n,r,a;
+     printf("enter the how many terms are present in the geometric progression=");
+	scanf("%d",&n);
+	printf("enter the 1st term in the gp=");
+	scanf("%d",&a);
+    printf("enter the common ratio of your geomatric progression=");
+	scanf("%d",&r);
+	int sum;
+	sum=a*((pow(r,n)-1)/(r-1));
+	printf("sum of the elements in the given gp is= %d",sum);
+}
